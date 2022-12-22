aa13c53abdb6036e566e2af2938ec6dc36708508
4Ci-Caliari-Nicola-2022-2023 <118118660+4Ci-Caliari-Nicola-2022-2023@users.noreply.github.com>
2022-12-22 16:32:08 +0100

    Delete caliari_nicola_televisore directory

--- caliari_nicola_televisore/bin/VERSIONE_1/TelevisoreTest.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class TelevisoreTest{
-    public static void main (String [] args){
-        Scanner input = new Scanner(System.in);
-        Televisore tv = new Televisore(27, "8k", "black");
-        System.out.print(tv.toString());
-    }
-}
